055ad62010b4956ef454252dda2107dacd1fb30e
Jastin Jay Panoncio <jastinpanoncio22@gmail.com>
2020-11-27 16:18:08 +0800

    commit on 'jastin'

--- lib/main.dart
+++ lib/main.dart
@@ -5,109 +5,155 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'MySchool App',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
+        primaryColor: Colors.amber,
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: StudentHome(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
+class StudentHome extends StatelessWidget {
+  Widget cardLists = ListView(
+    children: [
+      Container(
+          margin: EdgeInsets.fromLTRB(20.0, 0, 0, 0),
+          child: Row(
+            children: [
+              RaisedButton(
+                onPressed: () {},
+                child: Text(
+                  'My School',
+                  style: TextStyle(color: Colors.white),
+                ),
+                color: Colors.blue[900],
+              ),
+              RaisedButton(
+                onPressed: () {},
+                child: Text(
+                  'Announcements',
+                  style: TextStyle(color: Colors.white),
+                ),
+                color: Colors.blue[900],
+              ),
+              RaisedButton(
+                onPressed: () {},
+                child: Text(
+                  'About',
+                  style: TextStyle(color: Colors.white),
+                ),
+                color: Colors.blue[900],
+              )
+            ],
+          )),
+      Container(
+        child: Card(
+            margin: EdgeInsets.fromLTRB(20.0, 0, 20.0, 5.0),
+            shadowColor: Colors.grey,
+            borderOnForeground: true,
+            elevation: 2,
+            child: Column(
+              children: [
+                Image.asset('assets/images/My_School_Student_Hub.png'),
+                Padding(
+                  padding: const EdgeInsets.all(10.0),
+                  child: Text('Student Hub'),
+                )
+              ],
+            )),
+      ),
+      Container(
+        child: Card(
+            margin: EdgeInsets.fromLTRB(20.0, 5.0, 20.0, 5.0),
+            shadowColor: Colors.grey,
+            borderOnForeground: true,
+            elevation: 2,
+            child: Column(
+              children: [
+                Image.asset('assets/images/My_School_Study_Buddy.png'),
+                Padding(
+                  padding: const EdgeInsets.all(10.0),
+                  child: Text('Study Buddy'),
+                )
+              ],
+            )),
+      ),
+      Container(
+        child: Card(
+            margin: EdgeInsets.fromLTRB(20.0, 5.0, 20.0, 5.0),
+            shadowColor: Colors.grey,
+            borderOnForeground: true,
+            elevation: 2,
+            child: Column(
+              children: [
+                Image.asset('assets/images/My_School_LRC_Online.png'),
+                Padding(
+                  padding: const EdgeInsets.all(10.0),
+                  child: Text('LRC Online'),
+                )
+              ],
+            )),
+      ),
+      Container(
+        child: Card(
+            margin: EdgeInsets.fromLTRB(20.0, 5.0, 20.0, 5.0),
+            shadowColor: Colors.grey,
+            borderOnForeground: true,
+            elevation: 2,
+            child: Column(
+              children: [
+                Image.asset('assets/images/My_School_Kumustahan.png'),
+                Padding(
+                  padding: const EdgeInsets.all(10.0),
+                  child: Text('Kumustahan'),
+                )
+              ],
+            )),
+      )
+    ],
+  );
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+        leading: IconButton(
+          icon: Icon(Icons.list),
+          onPressed: () {
+            print('List Pressed');
+          },
+          color: Colors.amber,
+        ),
+        title: Text('My School',
+            style: TextStyle(color: Colors.amber, fontWeight: FontWeight.bold)),
+        backgroundColor: Colors.blue[900],
+        actions: [
+          IconButton(
+            icon: Icon(Icons.help_outline),
+            color: Colors.amber,
+            onPressed: () {
+              print('Help Clicked!');
+            },
+          )
+        ],
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
+      body: cardLists,
+      floatingActionButton: FloatingActionButton(
+        focusElevation: 5,
+        backgroundColor: Colors.blue[900],
+        onPressed: () {
+          print('Chat Clicked!');
+        },
+        child: Icon(
+          Icons.chat,
+          color: Colors.amber,
+          size: 25,
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
